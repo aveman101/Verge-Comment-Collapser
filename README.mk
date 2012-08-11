256d5358818efa679b0ec064bee21674ba12abcf
Avery <aapierce0@gmail.com>
2012-08-10 20:28:54 -0500

    Added readme

---
+++ README.mk
@@ -0,0 +1,14 @@
+This little extension does one thing, and one thing only: it adds buttons to collapse comments on theverge.com!
+
+It works in the comments section of articles, and also in the Verge forums.
+
+The buttons themselves are designed entirely with CSS, which means that they'll look super crisp even on your Retina Display.
+
+Install
+=======
+
+Chrome: from the [Chrome Web Store](https://chrome.google.com/webstore/detail/mafmbkmnkdhabjjenmpjcbpdfagpkmfk?hl=en-US)
+
+Safari: from the [downloads tab here on github](https://github.com/downloads/aveman101/Verge-Comment-Collapser/Verge%20Comment%20Collapser.safariextz)
+
+Firefox, Opera, and other browses are not (yet) supported. If you are talented in creating extensions for these browsers, *please*, *please*, *please* look over this code (it's not a lot), build an extenion, and share (I would do it myself, but the Firefox extension API overwhlemed me, and it seems that so few people use Opera that I didn't bother). I will be **more than happy** to feature those extensions here.
